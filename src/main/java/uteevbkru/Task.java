8541b7df4a9f84e58e43d5c9f165e4c1a8af52c1
Tony Uteev <uteev@bk.ru>
2017-03-10 17:35:12 +0300

    add function Task

--- src/main/java/uteevbkru/Task.java
+++ src/main/java/uteevbkru/Task.java
@@ -1,5 +1,22 @@
 package uteevbkru;
 
 public class Task {
-
+	
+	private String name;
+	private int priority;
+	
+	public Task(String name, int prior){
+		this.name = name;
+		priority = prior;
+	}
+	public Task(String name){
+		this.name = name;
+		priority = -1;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getPriority(){
+		return priority;
+	}
 }
--- src/main/java/uteevbkru/TaskPlanner.java
+++ src/main/java/uteevbkru/TaskPlanner.java
@@ -2,4 +2,14 @@ package uteevbkru;
 
 public class TaskPlanner {
 
+	public void pushTask(){
+		
+	}
+	public Task pullTask(){
+		return new Task("do");
+	}
+	public int sizeQueue(){
+		return 1;
+	}
+	
 }
